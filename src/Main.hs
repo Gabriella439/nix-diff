20c2868462fa0d963b56671df7a4ed8506282210
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-09-30 16:36:28 -0700

    Diff environment variables if dependencies are the same

--- src/Main.hs
+++ src/Main.hs
@@ -4,16 +4,19 @@
 
 module Main where
 
-import Control.Monad (forM_)
+import Control.Monad (forM, forM_)
+import Data.Algorithm.Diff (Diff(..))
 import Data.Attoparsec.Text (IResult(..))
 import Data.Map (Map)
 import Data.Monoid ((<>))
 import Data.Set (Set)
 import Data.Text (Text)
 import Filesystem.Path (FilePath)
+import Nix.Derivation (Derivation)
 import Options.Generic (Generic, ParseRecord)
 import Prelude hiding (FilePath)
 
+import qualified Data.Algorithm.Diff
 import qualified Data.Attoparsec.Text
 import qualified Data.Map
 import qualified Data.Set
@@ -44,15 +47,13 @@ groupByName m = Data.Map.fromList assocs
 
     assocs = fmap toAssoc (Data.Map.keys m)
 
-readInputs :: FilePath -> IO (Map Text (Map FilePath (Set Text)))
+readInputs :: FilePath -> IO Derivation
 readInputs path = do
     let string = Filesystem.Path.CurrentOS.encodeString path
     text <- Data.Text.IO.readFile string
-    derivation <- do
-        case Data.Attoparsec.Text.parse Nix.Derivation.parseDerivation text of
-            Done _ derivation -> return derivation
-            _                 -> fail "Could not parse derivation"
-    return (groupByName (Nix.Derivation.inputDrvs derivation))
+    case Data.Attoparsec.Text.parse Nix.Derivation.parseDerivation text of
+        Done _ derivation -> return derivation
+        _                 -> fail "Could not parse derivation"
 
 innerJoin :: Ord k => Map k a -> Map k b -> Map k (a, b)
 innerJoin = Data.Map.mergeWithKey both left right
@@ -63,11 +64,17 @@ innerJoin = Data.Map.mergeWithKey both left right
 
     right _ = Data.Map.empty
 
-plus :: Text -> Text
-plus text = "\ESC[1;32m+ " <> text <> "\ESC[0m"
+red :: Text -> Text
+red text = "\ESC[1;31m" <> text <> "\ESC[0m"
+
+green :: Text -> Text
+green text = "\ESC[1;32m" <> text <> "\ESC[0m"
 
 minus :: Text -> Text
-minus text = "\ESC[1;31m- " <> text <> "\ESC[0m"
+minus text = red ("- " <> text)
+
+plus :: Text -> Text
+plus text = green ("+ " <> text)
 
 explain :: Text -> Text
 explain text = "• " <> text
@@ -80,6 +87,18 @@ diffWith l r k = do
 renderOutputs :: Set Text -> Text
 renderOutputs outputs =
     ":{" <> Data.Text.intercalate "," (Data.Set.toList outputs) <> "}"
+
+diffText :: Text -> Text -> Text
+diffText left right = Data.Text.concat (fmap renderChunk chunks)
+  where
+    leftString  = Data.Text.unpack left
+    rightString = Data.Text.unpack right
+
+    chunks = Data.Algorithm.Diff.getGroupedDiff leftString rightString
+
+    renderChunk (First  l) = green (Data.Text.pack l)
+    renderChunk (Second r) = red   (Data.Text.pack r)
+    renderChunk (Both l _) = Data.Text.pack l
 
 diff :: Int -> FilePath -> Set Text -> FilePath -> Set Text -> IO ()
 diff indent leftPath leftOutputs rightPath rightOutputs = do
@@ -94,8 +113,10 @@ diff indent leftPath leftOutputs rightPath rightOutputs = do
             echo (explain "The derivation names do not match")
 
         else do
-            leftInputs  <- readInputs leftPath
-            rightInputs <- readInputs rightPath
+            leftDerivation  <- readInputs leftPath
+            rightDerivation <- readInputs rightPath
+            let rightInputs = groupByName (Nix.Derivation.inputDrvs leftDerivation)
+            let leftInputs  = groupByName (Nix.Derivation.inputDrvs rightDerivation)
     
             let leftNames  = Data.Map.keysSet leftInputs
             let rightNames = Data.Map.keysSet rightInputs
@@ -110,14 +131,14 @@ diff indent leftPath leftOutputs rightPath rightOutputs = do
                         echo ("    " <> sign name)
             else do 
                 let assocs = Data.Map.toList (innerJoin leftInputs rightInputs)
-                forM_ assocs $ \(inputName, (leftPaths, rightPaths)) -> do
+                descended <- forM assocs $ \(inputName, (leftPaths, rightPaths)) -> do
                     let leftExtraPaths =
                             Data.Map.difference leftPaths  rightPaths
                     let rightExtraPaths =
                             Data.Map.difference rightPaths leftPaths
                     case (Data.Map.toList leftExtraPaths, Data.Map.toList rightExtraPaths) of
                         _   | leftPaths == rightPaths -> do
-                            return ()
+                            return False
                         ([(leftPath', leftOutputs')], [(rightPath', rightOutputs')])
                             | leftOutputs' == rightOutputs' -> do
                             let [(leftPath' , leftOutputs' )] =
@@ -126,11 +147,30 @@ diff indent leftPath leftOutputs rightPath rightOutputs = do
                                     Data.Map.toList rightExtraPaths
                             echo (explain ("The input named `" <> inputName <> "` differs"))
                             diff (indent + 2) leftPath' leftOutputs' rightPath' rightOutputs'
+                            return True
                         _ -> do
                             echo (explain ("The set of inputs named `" <> inputName <> "` do not match"))
                             diffWith leftExtraPaths rightExtraPaths $ \(sign, extraPaths) -> do
                                 forM_ (Data.Map.toList extraPaths) $ \(extraPath, outputs) -> do
                                     echo ("    " <> sign (pathToText extraPath <> renderOutputs outputs))
+                            return False
+                if or descended
+                then return ()
+                else do
+                    let leftEnv  = Nix.Derivation.env leftDerivation
+                    let rightEnv = Nix.Derivation.env rightDerivation
+                    let leftExtraEnv =
+                            Data.Map.difference leftEnv rightEnv
+                    let rightExtraEnv =
+                            Data.Map.difference leftEnv rightEnv
+                    let bothEnv = innerJoin leftEnv rightEnv
+                    diffWith leftExtraEnv rightExtraEnv $ \(sign, extraEnv) -> do
+                        forM_ (Data.Map.toList extraEnv) $ \(key, value) -> do
+                            echo (sign (key <> "=" <> value))
+                    forM_ (Data.Map.toList bothEnv) $ \(key, (leftValue, rightValue)) -> do
+                        if leftValue == rightValue
+                        then return ()
+                        else echo (key <> "=" <> diffText leftValue rightValue)
   where
     echo text = Data.Text.IO.putStrLn (Data.Text.replicate indent " " <> text)
 
