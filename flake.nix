0f4f1f8ac991b5f21ce76cb3e3c730c177cd1749
Gabriella Gonzalez <Gabriel439@gmail.com>
2022-11-30 17:39:51 -0800

    Additional CI fixes
    - Remove obsolete ci.* files
    
      … as suggested by @Player-205: https://github.com/Gabriella439/nix-diff/pull/62#issuecomment-1331897039
    
    - Add garnix substituter to `flake.nix`

--- flake.nix
+++ flake.nix
@@ -37,4 +37,12 @@
           devShells.default = pkgs.haskellPackages.nix-diff.env;
         }
     );
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
 }
