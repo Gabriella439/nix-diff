5741915bc6bbcfd02fb3ce59921921b9e21a6315
Sandro Jäckel <sandro.jaeckel@sap.com>
2023-02-07 11:23:36 +0100

    Expose overlay
    config and overlay was moved outside of forAllSystems because it is
    architecture independent.

--- flake.nix
+++ flake.nix
@@ -1,49 +1,52 @@
-{ inputs = {
+{
+  inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
+    let
+      config = { };
+
+      overlay = pkgsNew: pkgsOld: {
+        nix-diff =
+          pkgsNew.haskell.lib.justStaticExecutables
+            pkgsNew.haskellPackages.nix-diff;
+
+        haskellPackages = pkgsOld.haskellPackages.override (old: {
+          overrides = self: super: {
+            nix-diff =
+              # There are quick check and golden tests.
+              # Quick check tests require random source to work.
+              # Golden tests require write access to the /nix/var
+              # and read access to the test data into /nix/store
+              # So we can't run these tests in build time
+              pkgsNew.haskell.lib.dontCheck
+                (self.callCabal2nix "nix-diff" ./. { });
+          };
+        });
+      };
+    in
+    {
+      overlays.default = overlay;
+    } //
+    (utils.lib.eachDefaultSystem (system:
       let
-        config = { };
+        pkgs = import nixpkgs { inherit config system; overlays = [ overlay ]; };
+      in
+      {
+        packages.default = pkgs.haskellPackages.nix-diff;
 
-        overlay = pkgsNew: pkgsOld: {
-          nix-diff =
-            pkgsNew.haskell.lib.justStaticExecutables
-              pkgsNew.haskellPackages.nix-diff;
+        apps.default = {
+          type = "app";
 
-          haskellPackages = pkgsOld.haskellPackages.override (old: {
-            overrides = self: super: {
-              nix-diff =
-                # There are quick check and golden tests.
-                # Quick check tests require random source to work.
-                # Golden tests require write access to the /nix/var
-                # and read access to the test data into /nix/store
-                # So we can't run these tests in build time
-                pkgsNew.haskell.lib.dontCheck
-                (self.callCabal2nix "nix-diff" ./. {});
-            };
-          });
+          program = "${pkgs.nix-diff}/bin/nix-diff";
         };
 
-        pkgs =
-          import nixpkgs { inherit config system; overlays = [ overlay ]; };
-
-      in
-        rec {
-          packages.default = pkgs.haskellPackages.nix-diff;
-
-          apps.default = {
-            type = "app";
-
-            program = "${pkgs.nix-diff}/bin/nix-diff";
-          };
-
-          devShells.default = pkgs.haskellPackages.nix-diff.env;
-        }
-    );
+        devShells.default = pkgs.haskellPackages.nix-diff.env;
+      }
+    ));
 
   nixConfig = {
     extra-substituters = [ "https://cache.garnix.io" ];
