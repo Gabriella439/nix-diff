6b5daad04c586dfae51b747fd187de1c9d37b1ee
Richard Marko <srk@48.io>
2023-11-20 07:28:19 +0100

    flake.nix: drop nixConfig with garnix cache

--- flake.nix
+++ flake.nix
@@ -47,12 +47,4 @@
         devShells.default = pkgs.haskellPackages.nix-diff.env;
       }
     ));
-
-  nixConfig = {
-    extra-substituters = [ "https://cache.garnix.io" ];
-
-    extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-  };
 }
