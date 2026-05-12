cf8f902b877fead5c0c691285da7b6f7319b1f52
Sorki <srk@48.io>
2026-05-12 15:31:11 +0200

    Update CI (#106)

--- ci.sh
+++ ci.sh
@@ -6,5 +6,5 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 echo "regenerating .github/workflows/ci.yaml"
 
-which dhall-to-yaml || cabal install dhall-yaml
+which dhall-to-yaml-ng || cabal install dhall-yaml
 dhall-to-yaml-ng --generated-comment --file ci.dhall > .github/workflows/ci.yaml
